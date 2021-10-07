f338e71fbfa7aca139c58050b58180795d1157b6
Alexander Chernykh <al@Alexanders-MacBook-Air.local>
2021-10-07 15:40:34 +0300

    added html and js

---
+++ script.js
@@ -0,0 +1,33 @@
+let expression = "Hi my name is mud";
+
+// let search = expression.includes("mudd");
+//let found = expression.indexOf("is");
+// let beginning = expression.startsWith("Hi");
+// let newString = expression.slice(6, 10);
+// let upper = expression.toUpperCase();
+// let lower = expression.toLowerCase();
+
+let searchText = "     whoever said    ";
+
+console.log(searchText.trim());
+
+
+
+
+
+
+
+
+
+
+
+// let something = {
+//     title: "salesforce consultant",
+//     exp: "5 years",
+//     sal: "5k"
+// }
+
+// let {title, exp} = something;
+
+// console.log(title);
+// console.log(exp);
